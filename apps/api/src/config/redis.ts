2e4e1eaae64b3a073f4e14dba6e668cb3e50db0f
arcane_2004 <sumitkp153@gmail.com>
2026-08-02 02:48:59 +0530

    added config.redis in api

---
+++ apps/api/src/config/redis.ts
@@ -0,0 +1,17 @@
+import {Redis} from "ioredis";
+
+export const redis = new Redis({
+    host: process.env.REDIS_HOST || "localhost",
+
+    port: Number(process.env.REDIS_PORT) || 6379,
+
+    password: process.env.REDIS_PASSWORD || undefined,
+});
+
+redis.on("connect", () => {
+    console.log("✅ Connected to Redis");
+});
+
+redis.on("error", (error: any) => {
+    console.error("❌ Redis Error:", error);
+});
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { redis } from "./config/redis.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT ;
+
+await redis.ping();
+
+console.log("Redis ping successful");
 
 app.listen(PORT, () => {
   console.log(`🚀 API Server running on http://localhost:${PORT}`);
 });
+
