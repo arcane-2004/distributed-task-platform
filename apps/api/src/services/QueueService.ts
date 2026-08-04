449187f30a54f6243c5e06ff35284d3cb51f7629
arcane_2004 <sumitkp153@gmail.com>
2026-08-04 15:18:24 +0530

    added queue service submit job method

---
+++ apps/api/src/services/QueueService.ts
@@ -0,0 +1,16 @@
+import { SubmitJobRequest } from "../../../../shared/queue-engine/models/SubmitJobRequest.js";
+import { Job } from "../../../../shared/queue-engine/models/Job.js";
+
+import { queueEngine } from "../config/queueEngine.js";
+
+export class QueueService {
+
+    async submitJob(
+        request: SubmitJobRequest
+    ): Promise<Job> {
+
+        return await queueEngine.submit(request);
+
+    }
+
+}
