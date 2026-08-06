7f4fc0aa6e93e714d528e1ff6c5e3c60b94a8871
arcane_2004 <sumitkp153@gmail.com>
2026-08-06 19:06:36 +0530

    created: worker- email job handler

--- apps/worker/src/handlers/EmailJobHandler.ts
+++ apps/worker/src/handlers/EmailJobHandler.ts
@@ -1,14 +1,32 @@
 import { JobHandler } from "../../../../shared/queue-engine/handlers/JobHandler.js";
 import { Job } from "../../../../shared/queue-engine/models/Job.js";
 
+interface EmailPayload {
+    to: string;
+    subject: string;
+    body: string;
+}
 export class EmailJobHandler implements JobHandler {
 
-    async execute(
-        job: Job
-    ): Promise<void> {
+    async execute(job: Job<EmailPayload>): Promise<void> {
 
-        console.log("Processing EMAIL job:");
-        console.log(job);
+        const {
+            to,
+            subject,
+            body
+        } = job.payload;
+
+        console.log("=================================");
+        console.log("Processing Email Job");
+        console.log(`Job ID : ${job.id}`);
+        console.log(`To     : ${to}`);
+        console.log(`Subject: ${subject}`);
+        console.log("=================================");
+
+        console.log("Preparing email...");
+
+        console.log("Email prepared successfully.");
+
     }
 
 }
