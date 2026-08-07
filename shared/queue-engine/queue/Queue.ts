764f74254f394744a584bcb8939717d190de74de
arcane_2004 <sumitkp153@gmail.com>
2026-08-07 11:36:34 +0530

    import bug

--- shared/queue-engine/queue/Queue.ts
+++ shared/queue-engine/queue/Queue.ts
@@ -1,5 +1,5 @@
-import Redis from "ioredis";
-import { RedisKeys } from "../keys/RedisKeys";
+import {Redis} from "ioredis";
+import {RedisKeys } from "../keys/RedisKeys.js";
 
 export class Queue {
 
